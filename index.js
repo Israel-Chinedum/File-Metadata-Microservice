858d1032709673940904b1cc11e7502a444c91b5
Israel Favour <fastarfavour@gmail.com>
2025-01-08 15:32:27 -0800

    made changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var multer = require('multer'); 
 var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
+const upload = multer();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,9 +14,16 @@ app.get('/', function (req, res) {
 });
 
 
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size
+    });
 });
